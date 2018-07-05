387b323f7e0cbb348224b3fa06d9819133be2ece
bencode <bencode@163.com>
2018-07-05 11:35:09 +0800

    chapter 8

---
+++ the-little-schemer/8-lambda-the-ultimate.rkt
@@ -0,0 +1,78 @@
+(define rember-f
+  (lambda (test? a l)
+    (cond
+      ((null? l) '())
+      ((test? a (car l)) (rember-f test? a (cdr l)))
+      (else
+       (cons (car l) (rember-f test? a (cdr l)))))))
+
+
+(rember-f eq? 'jelly '(jelly beans are good))
+(rember-f equal? 'are '(jelly beans are good))
+
+
+; curring-ing
+(define eq?-c
+  (lambda (a)
+    (lambda (x)
+      (eq? x a))))
+
+(define eq?-salad (eq?-c 'salad))
+(eq?-salad 'salad)
+
+((eq?-c 'salad) 'salad)
+
+
+(define rember-ff
+  (lambda (test?)
+    (lambda (a l)
+      (cond
+        ((null? l) '())
+        ((test? a (car l)) (cdr l))
+        (else
+         (cons
+          (car l)
+          ((rember-ff test?) a (cdr l))))))))  ;有办法可以直接引用到匿名函数吗？
+
+(define rember-eq? (rember-ff eq?))
+(rember-eq? 'salad '(tuna salad is good))
+
+
+(define insert-ff
+  (lambda (build)
+    (lambda (test?)
+      (lambda (new old l)
+        (cond
+          ((null? l) '())
+          ((test? old (car l)) (build new old l))
+          (else (cons (car l)
+                      (((insert-ff build) test?) new old (cdr l)))))))))
+
+(define insert-gg
+  (lambda (build)
+    ((insert-ff build) eq?)))
+
+(define insertL
+  (insert-gg
+   (lambda (new old l)
+     (cons new (cons old (cdr l))))))
+
+
+(insertL 'a 'b '(b c d))
+; 这种书写方式应该如何才能更容易理解呢
+
+
+(define subst
+  (insert-gg
+   (lambda (new old l)
+     (cons new (cdr l)))))
+; insert-ff --->
+
+(subst 'a 'd '(b a d c))
+
+;;; The Ninth Commandment
+;;; Abstract common patterns with a new functions.
+
+
+
+
